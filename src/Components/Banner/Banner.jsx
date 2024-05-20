0f7b978ffab7ffe2c04c18a4166e8df769c97059
vanaf25 <vanayfefilov777@gmail.com>
2024-05-20 19:43:49 +0200

    first commit

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,24 +2,29 @@
 
 import React from 'react';
 import styles from './Banner.module.css';
-import {Box, Container} from "@mui/material";
+import {Box} from "@mui/material";
 import BlackButton, {BlackButton2} from "../../common/BlackButton/BlackButton";
+import Container from "../../common/Container/Container";
 
 const CustomElement = () => {
     return (
-        <div  className={`${styles.container}`}>
-            <img
-                className={styles.background__image}
-                src="https://cdn.medexpress.pl/media/images/uploads202208iStock-649824716-480x.original.format-webp_VBQAdfX.webp" alt="Repo"/>
-            <div className={`${styles.content} ${styles.overlay}`}>
-                <Box>
-                    <h1>NOWOSC</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    <BlackButton2 variant={"outlined"}>Button</BlackButton2>
-                </Box>
+        <Container>
+            <div className={`${styles.container}`}>
+                <img
+                    className={styles.background__image}
+                    src="https://cdn.medexpress.pl/media/images/uploads202208iStock-649824716-480x.original.format-webp_VBQAdfX.webp"
+                    alt="Repo"/>
+                <div className={`${styles.content} ${styles.overlay}`}>
+                    <Box>
+                        <h1>NOWOSC</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <BlackButton2 variant={"outlined"}>Button</BlackButton2>
+                    </Box>
 
+                </div>
             </div>
-        </div>
+        </Container>
+
     );
 };
 
