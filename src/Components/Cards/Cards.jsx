2526eead1cb4a96a2523687545a861e8b425216d
vanaf25 <vanayfefilov777@gmail.com>
2024-05-20 20:55:00 +0200

    first commit

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -5,17 +5,14 @@ import Container from "../../common/Container/Container";
 const Cards = ({related}) => {
     return (
         <Container>
-            <ul style={{listStyle: "none",margin:"0 0 10px 0", padding:0}}>
-                <Grid container spacing={{xs: 2, md: 3}} columns={{xs: 4, sm: 8, md: 12}}>
+                <Grid component={"ul"} style={{listStyle:"none"}} container spacing={{xs: 2, md: 3}}
+                      columns={{xs: 4, sm: 8, md: 12}}>
                     {Array.from(Array(6)).map((_, index) => (
-                        <Grid item xs={4} sm={4} md={4} key={index}>
-                            <li className={"item"}>
+                        <Grid className={"item"} component={"li"}  item xs={4} sm={4} md={4} key={index}>
                                 <Card/>
-                            </li>
                         </Grid>
                     ))}
                 </Grid>
-            </ul>
             {!related && <Pagination count={10}/>}
         </Container>
 
